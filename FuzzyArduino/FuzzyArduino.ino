d7c3a3db6b472bf4d6e81024a69036fccf6cca42
cimbriano <Christopher.Imbriano@gmail.com>
2014-02-19 18:17:18 -0500

    Adds handling for A2 and changes Serial write format to #,#,#,#

--- FuzzyArduino/FuzzyArduino.ino
+++ FuzzyArduino/FuzzyArduino.ino
@@ -2,6 +2,9 @@
 int pressureInPin = 0;
 int pressureReading;
 
+int pressureInPin2 = 2;
+int pressureReading2;
+
 int touchMembraneInPin = 5;
 int touchMembraneReading;
 
@@ -11,12 +14,23 @@ void setup() {
 
 void loop() {
     pressureReading = analogRead(pressureInPin);
-    Serial.print("Pressure reading = ");
-    Serial.println(pressureReading);
+    // Serial.print("P1 = ");
+    Serial.print(pressureReading);
+    Serial.print(',');
+
+    pressureReading2 = analogRead(pressureInPin2);
+    // Serial.print("\tP2 = ");
+    Serial.print(pressureReading2);
+    Serial.print(',');
 
     touchMembraneReading = analogRead(touchMembraneInPin);
-    Serial.print("Touch Reading = ");
-    Serial.println(touchMembraneReading);
+    // Serial.print("\tT1 = ");
+    Serial.print(touchMembraneReading);
+    Serial.print(',');
+
+    // 4th sensor not set up yet
+    Serial.print(512);
+    Serial.print('\n');
 
     delay(750);
 }
